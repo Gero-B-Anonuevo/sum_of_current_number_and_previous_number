f854d31b61490d0601100a6f075eaa8e8317aced
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 11:28:38 +0800

    working

--- prog.py
+++ prog.py
@@ -2,11 +2,11 @@
 
 
 #assign value for previous number
-previous_number = 1
+previous_number = 0
+
 #make the range function
-for num in range(1, 10):
-    sum = num + previous_number
-    print(previous_number, num, sum)
+for current_num in range(1, 11):
+    sum = current_num + previous_number
+    print(previous_number, current_num, sum)
     #overwrite previous number to keep the process going
-    previous_number = num
-    
+    previous_number =current_num
