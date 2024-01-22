a847e6b9152c0ed54e7623a4a109de7d3acffed4
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 11:30:35 +0800

    tried to do the recommended solution same results

--- prog.py
+++ prog.py
@@ -1,12 +1,20 @@
 #Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number.
 
 
-#assign value for previous number
-previous_number = 0
+#previous_number = 0
 
-#make the range function
-for current_num in range(1, 11):
-    sum = current_num + previous_number
-    print(previous_number, current_num, sum)
-    #overwrite previous number to keep the process going
-    previous_number =current_num
+#for current_num in range(1, 11):
+#   sum = current_num + previous_number
+#    print(previous_number, current_num, sum)
+#    previous_number = current_num
+
+print("Printing current and previous number and their sum in a range(10)")
+previous_num = 0
+
+# loop from 1 to 10
+for i in range(1, 11):
+    x_sum = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
+    # modify previous number
+    # set it to the current number
+    previous_num = i
